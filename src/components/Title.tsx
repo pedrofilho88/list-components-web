0ee03ab4367a2700d7a21f44e0d19f82198ddf40
pedro filho oliveira santos <pedrinhosecundo@hotmail.com>
2022-06-19 12:22:11 -0300

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,6 @@
-export default function Title(props) {
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+
+export default function Title(props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) {
     return (
         <div className=" flex flex-col items-center justify-center " >
             <h1 className="px-4 py-2  text-2xl">
